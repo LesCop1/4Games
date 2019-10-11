6c65886f6ca82013a1ba3a1762e6c9fdce5526d0
Stelario <quentincarry@gmail.com>
2019-10-11 14:38:16 +0200

    Save 11/10

--- Quentin/src/batailleNavale/Game.java
+++ Quentin/src/batailleNavale/Game.java
@@ -5,7 +5,39 @@ import java.util.List;
 
 public class Game {
 
+    List<String> board = new ArrayList<>();
     List<Boat> listBoat = new ArrayList<>();
+
+    public void initBoard() {
+        for (int i = 0; i < 100; i++) {
+            board.add("o");
+        }
+    }
+
+    public void affichage(){
+        for(int i=0; i<board.size(); i++){
+            if(i%10==0) System.out.println();
+            System.out.print(board.get(i));
+        }
+    }
+
+    public void putBoat(Boat boat, int i) {
+        if (boat.isOrientation()) {
+            for(int j=i; j<boat.getSizeBoat(); j++){
+                board.set(j,"A");
+            }
+        }else{
+            System.out.println("ALED");
+        }
+    }
+
+    public void swapOrientation(Boat boat) {
+        if (boat.isOrientation()) {
+            boat.setOrientation(false);
+        } else {
+            boat.setOrientation(true);
+        }
+    }
 
     public void genListBoat() {
         listBoat.add(Boat.A);
@@ -16,9 +48,9 @@ public class Game {
         System.out.println(listBoat);
     }
 
-    public int deleteBoat(Boat b){
+    public int deleteBoat(Boat b) {
         listBoat.remove(b);
-        if(listBoat.isEmpty()){
+        if (listBoat.isEmpty()) {
             dead();
             System.out.println("gg");
             System.exit(1);
@@ -27,33 +59,21 @@ public class Game {
         return 0;
     }
 
-    public void touch(Boat boat){
+    public void touch(Boat boat) {
         boat.setSizeBoat(-1);
         sink(boat);
     }
 
-    public void sink(Boat boat){
-        if(boat.getSizeBoat()==0){
+    public void sink(Boat boat) {
+        if (boat.getSizeBoat() == 0) {
             boat.setAlive(false);
             deleteBoat(boat);
         }
     }
 
-    public void putBoat(){
-        //TODO Placement bateau en fonction de la grille
-    }
-
-    public void swapOrientation(Boat boat){
-        if(boat.isOrientation()){
-            boat.setOrientation(false);
-        }else{
-            boat.setOrientation(true);
-        }
-    }
-
-    public void dead(){
+    public void dead() {
         Boat.stream()
-                .filter(d -> d.isAlive()==false)
+                .filter(d -> d.isAlive() == false)
                 .forEach(System.out::println);
     }
 }
--- Quentin/src/batailleNavale/Main.java
+++ Quentin/src/batailleNavale/Main.java
@@ -3,11 +3,14 @@ package batailleNavale;
 public class Main {
     public static void main(String[] args) {
         Game g = new Game();
-        g.genListBoat();
-        g.touch(Boat.T);
-        g.touch(Boat.A);
-        g.touch(Boat.C);
-        g.touch(Boat.F);
-        g.touch(Boat.S);
+        g.initBoard();
+        g.putBoat(Boat.A,1);
+        g.affichage();
+//        g.genListBoat();
+//        g.touch(Boat.T);
+//        g.touch(Boat.A);
+//        g.touch(Boat.C);
+//        g.touch(Boat.F);
+//        g.touch(Boat.S);
     }
 }
