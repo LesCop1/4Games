1aa264250ff7dc0cf0712c69593d40dfced96a85
BuildTools <unconfigured@null.spigotmc.org>
2019-10-25 13:16:40 +0200

    Switch PC

--- src/main/java/fr/bcecb/bingo/BingoState.java
+++ src/main/java/fr/bcecb/bingo/BingoState.java
@@ -7,25 +7,32 @@ import java.util.List;
 import java.util.Random;
 
 public class BingoState extends ScreenState {
-    private final int nbGrids;
+    private int nbGrids;
     private List<Integer> numberList = new ArrayList<>();
     private List<Player> bots = new ArrayList<>();
     private final static int NB_BOTS = 5;
+    private Player player;
+    private int lastDrop;
 
     public BingoState(int nbGrids) {
         super("bingo");
         this.nbGrids = nbGrids;
-        Grid g = new Grid();
-        Player p = new Player(nbGrids);
+        initGame();
+        createBots();
+    }
 
+    private void initGame(){
+        player = new Player(nbGrids);
         int i = 1;
         while (i < 91) {
             numberList.add(i - 1, i);
             i++;
         }
+    }
 
+    private void createBots(){
         Random rand = new Random();
-        for (int j = 0; j < NB_BOTS; i++) {
+        for (int j = 0; j < NB_BOTS; j++) {
             Player bot = new Player((rand.nextInt(5)+1));
             bots.add(bot);
         }
@@ -46,13 +53,15 @@ public class BingoState extends ScreenState {
     @Override
     public void update() {
         super.update();
+        dropball();
+    }
 
-        // Drop Number
+    private void dropball(){
         Random rand = new Random();
         int randInt = rand.nextInt(numberList.size() - 1 + 1) + 1;
         numberList.get(randInt);
         numberList.remove(randInt);
-        //
+        this.lastDrop = randInt;
     }
 
     @Override
