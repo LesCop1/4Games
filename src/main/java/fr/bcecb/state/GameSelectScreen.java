2615d2c8a14dbf7492af880120278929cab6576d
Mathis <mathisengels@yahoo.fr>
2019-11-07 00:38:04 +0100

    Basic GUI

--- src/main/java/fr/bcecb/state/GameSelectScreen.java
+++ src/main/java/fr/bcecb/state/GameSelectScreen.java
@@ -2,7 +2,12 @@ package fr.bcecb.state;
 
 import fr.bcecb.Game;
 import fr.bcecb.event.MouseEvent;
+import fr.bcecb.render.Window;
+import fr.bcecb.resources.ResourceHandle;
+import fr.bcecb.resources.Texture;
 import fr.bcecb.state.gui.Button;
+import fr.bcecb.state.gui.CircleButton;
+import fr.bcecb.state.gui.GuiElement;
 import fr.bcecb.state.gui.ScreenState;
 
 public class GameSelectScreen extends ScreenState {
@@ -12,12 +17,40 @@ public class GameSelectScreen extends ScreenState {
 
     @Override
     public void initGui() {
-        addGuiElement(new Button(0, 0, 0, 100, 100) {
-            @Override
-            protected void onClick(MouseEvent.Click event) {
-                Game.instance().getStateEngine().popState();
-            }
+        int width = Window.getCurrentWindow().getWidth();
+        int height = Window.getCurrentWindow().getHeight();
+        setBackgroundTexture(new ResourceHandle<Texture>("textures/mainMenuBG.png") {});
+
+        GuiElement sudokuGameButton = new Button(0, (width / 4f), (height / 2f) - (height / 10f), (width / 8f), (height / 10f),
+                true, "Sudoku", new ResourceHandle<Texture>("textures/defaultButton.png") {
         });
+
+        GuiElement bingoGameButton = new Button(1, (width / 4f), (height / 2f) + (height / 10f), (width / 8f), (height / 10f),
+                true, "Bingo", new ResourceHandle<Texture>("textures/defaultButton.png") {
+        });
+
+        GuiElement profileButton = new CircleButton(2, width / 2.0f, height / 2.0f, (height / 10f), true,
+                new ResourceHandle<Texture>("textures/defaultProfile.png") {
+                }).setClickHandler(e -> Game.instance().getStateEngine().pushState(new ProfileScreen()));
+
+        GuiElement bsGameButton = new Button(3, (width / 4f) * 3, (height / 2f) - (height / 10f), (width / 8f), (height / 10f),
+                true, "Battle Ship", new ResourceHandle<Texture>("textures/defaultButton.png") {
+        });
+
+        GuiElement pokerGameButton = new Button(4, (width / 4f) * 3, (height / 2f) + (height / 10f), (width / 8f), (height / 10f),
+                true, "Poker", new ResourceHandle<Texture>("textures/defaultButton.png") {
+        });
+
+        GuiElement backButton = new Button(5, (width / 20f), (height - (height / 20f) - (height / 10f)), (height / 10f),
+                (height / 10f), false, "Back", new ResourceHandle<Texture>("textures/defaultButton.png") {
+        }).setClickHandler(e -> Game.instance().getStateEngine().popState());
+
+        addGuiElement(sudokuGameButton);
+        addGuiElement(bingoGameButton);
+        addGuiElement(pokerGameButton);
+        addGuiElement(bsGameButton);
+        addGuiElement(profileButton);
+        addGuiElement(backButton);
     }
 
     @Override
