5db92b265ca85736951372a988067c2519c5c8ee
Mathis <mathisengels@yahoo.fr>
2019-11-11 19:31:50 +0100

    redo the previous fix

--- src/main/java/fr/bcecb/input/MouseManager.java
+++ src/main/java/fr/bcecb/input/MouseManager.java
@@ -4,6 +4,7 @@ import fr.bcecb.Game;
 import fr.bcecb.event.Event;
 import fr.bcecb.event.MouseEvent;
 import fr.bcecb.render.Window;
+import org.lwjgl.system.Platform;
 
 import static org.lwjgl.glfw.GLFW.*;
 
@@ -45,8 +46,10 @@ public class MouseManager {
         this.lastPositionX = positionX;
         this.lastPositionY = positionY;
 
-        this.positionX = (float) x / Window.getCurrentWindow().getContentScaleX();
-        this.positionY = (float) y / Window.getCurrentWindow().getContentScaleY();
+        if (Platform.get() != Platform.MACOSX) {
+            this.positionX = (float) x / Window.getCurrentWindow().getContentScaleX();
+            this.positionY = (float) y / Window.getCurrentWindow().getContentScaleY();
+        }
 
         Event event = new MouseEvent.Move(positionX, positionY, lastPositionX - positionX, lastPositionY - positionY);
         Game.EVENT_BUS.post(event);
