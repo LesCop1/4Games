bd48d8d71b8e93267eb35649a5877b9553fbc732
Antoine Balieu <vhayn@outlook.com>
2019-12-13 13:46:45 +0100

    Changed color

--- src/main/java/fr/bcecb/util/Constants.java
+++ src/main/java/fr/bcecb/util/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     public static final Vector4f COLOR_WHITE = new Vector4f(1.0f);
     public static final Vector4f COLOR_BLACK = new Vector4f();
     public static final Vector4f COLOR_GREY = new Vector4f(0.5f, 0.5f, 0.5f, 1f);
-    public static final Vector4f COLOR_LIGHT_GREY = new Vector4f(0.8f, 0.8f, 0.8f, 1f);
+    public static final Vector4f COLOR_LIGHT_GREY = new Vector4f(0.5f, 0.5f, 0.5f, 1f);
     public static final Vector4f COLOR_RED = new Vector4f(1f, 0f, 0f, 1f);
     public static final Vector4f COLOR_GREEN = new Vector4f(0f, 1f, 0f, 1f);
 
